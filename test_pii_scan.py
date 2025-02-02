379fed594126bb069c4e6b134eb0c71764810e49
claesmk <44329220+claesmk@users.noreply.github.com>
2025-02-02 14:51:23 +0000

    Sprint2 updates

--- test_pii_scan.py
+++ test_pii_scan.py
@@ -1,3 +1,4 @@
+"""Tests for pii_scan"""
 import unittest
 import re
 import os
@@ -5,6 +6,7 @@ from pii_scan import show_aggie_pride, analyze_text
 
 
 class TestPIIScan(unittest.TestCase):
+    """Tests for the pii_scan module"""
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
@@ -12,7 +14,7 @@ class TestPIIScan(unittest.TestCase):
     def test_base_supported_entities(self):
         """Test to make sure the default supported entities are returned"""
         # read entities from team_entities.csv
-        with open('team_entities.csv') as f:
+        with open('team_entities.csv', encoding='utf-8') as f:
             entities = f.read().splitlines()
             # remove the header row
             entities = entities[1:]
@@ -23,6 +25,8 @@ class TestPIIScan(unittest.TestCase):
         for entity in supported_entities:
             if entity:
                 self.assertIn(entity, results)
+            # Check to make sure recognizer actually loads the entity
+                analyze_text('entity load test', entity_list=[entity])
 
     def test_starts_with_test(self):
         """Test to make sure all test methods start with test"""
@@ -31,10 +35,11 @@ class TestPIIScan(unittest.TestCase):
         # This is a common mistake that can cause tests to be skipped
         for file in os.listdir('.'):
             if file.endswith('.py') and file.startswith('test_'):
-                with open(file) as f:
+                with open(file, encoding='utf-8') as f:
                     for line in f:
                         # make sure everything that looks like a method name starts with test
                         m = re.search(r'\s*def (\w+)', line)
                         if m:
-                            self.assertTrue(m.group(1).startswith('test'),
-                                            'Method name does not start with test: def ' + m.group(1) + ' in ' + file)
+                            err = 'Method name does not start with test: def '
+                            err += m.group(1) + ' in ' + file
+                            self.assertTrue(m.group(1).startswith('test'), err)
