fe9907548587331fd86b436632e05db7bea84c39
Micah Aghedo <108535612+maghedo243@users.noreply.github.com>
2025-02-17 15:50:44 +0000

    Added test cases for IBAN detection

--- test_team_cmd.py
+++ test_team_cmd.py
@@ -1,6 +1,6 @@
 """Unit test file for team cmd"""
 import unittest
-from pii_scan import analyze_text, show_aggie_pride  # noqa 
+from pii_scan import analyze_text, show_aggie_pride  # noqa
 
 
 class TestTeam_cmd(unittest.TestCase):
@@ -20,6 +20,50 @@ class TestTeam_cmd(unittest.TestCase):
 
     def test_iban_code(self):
         """Test IBAN_CODE functionality"""
+        pos_test_cases = [
+                            "DE44500105175407324931", 
+                            "FR7630006000011234567890189",  
+                            "ES9121000418450200051332", 
+                            "IT60X0542811101000000123456"
+                        ]
+        neg_test_cases = [
+                        "DE4450010517540732",  # Too short
+                        "FR7630006000011234567890189123",  # Too long
+                        "IT60X0542811101O00000123456",  # Non-numeric character (O instead of 0)
+                        "NL91ABNA!417164300",  # Special character included (!)
+                        "GB29 NW BK60161331926819",  # Extra spaces
+                        "SA0280000000608010167519",  # Incorrect check digits
+                        "AE070331234567890123546"  # Swapped digits
+                    ]
+
+        #without context tests
+        for x in pos_test_cases:
+            results = analyze_text(f"{x}",["IBAN_CODE"])
+
+            self.assertGreater(len(results),0) #asserting a full list for a detected code
+
+            self.assertGreaterEqual(results[0].score, 0.5) #valid IBAN
+            self.assertEqual(results[0].entity_type, 'IBAN_CODE') #validating entity type
+
+        for x in neg_test_cases:
+            results = analyze_text(f"{x}",["IBAN_CODE"])
+            self.assertEqual(len(results),0) #asserting an empty list for an undetected code
+
+        
+        #with context tests
+        for x in pos_test_cases:
+            results = analyze_text(f"IBAN CODE: {x}",["IBAN_CODE"])
+
+            self.assertGreater(len(results),0) #asserting a full list for a detected code
+
+            self.assertGreaterEqual(results[0].score, 0.5) #valid IBAN
+            self.assertEqual(results[0].entity_type, 'IBAN_CODE') #validating entity type
+
+        for x in neg_test_cases:
+            results = analyze_text(f"IBAN CODE: {x}",["IBAN_CODE"])
+            self.assertEqual(len(results),0) #asserting an empty list for an undetected code
+
+        
 
     def test_ip_address(self):
         """Test IP_ADDRESS functionality"""
