a1f33e9cd4d7eab31dedcff47533213ae98d2a1d
danyellepalmore <94997942+danyellepalmore@users.noreply.github.com>
2025-02-19 05:38:34 +0000

    ES_NIE context enhancement

--- test_team_cmd.py
+++ test_team_cmd.py
@@ -1,6 +1,6 @@
 """Unit test file for team cmd"""
 import unittest
-from pii_scan import analyze_text, show_aggie_pride  # noqa 
+from pii_scan import analyze_text, show_aggie_pride  # noqa
 
 
 class TestTeam_cmd(unittest.TestCase):
@@ -11,24 +11,36 @@ class TestTeam_cmd(unittest.TestCase):
 
     def test_es_nie(self):
         """Test ES_NIE functionality"""
-        #positive test case *returning empty list
+        # positive test case *returning empty list
         prefix = 'X'
         middle = '1234567'
         suffix = 'A'
         test_str = prefix + middle + suffix
         result = analyze_text(test_str, ['ES_NIE'])
-        #expect value in result list
-        #self.assertGreater(len(result),0, 'Result is empty')
-        #checking entity type is actually first elements
-        #self.assertEqual(result[0].entity_type, 'ES_NIE')
+        # expect value in result list
+        self.assertGreater(len(result),0, 'Result is empty')
+        # checking entity type is actually first elements
+        self.assertEqual(result[0].entity_type, 'ES_NIE')
+        # check the score ?
+        self.assertEqual(result[0].score, 0.5)
 
-        #negative test case
-        #first letter is not X,Y, or Z
+        # context enhancement
+        # add context word
+        test_str = 'NIE' + test_str
+        result = analyze_text(test_str, ['ES_NIE'])
+        # expect value in result list
+        self.assertGreater(len(result),0, 'Result is empty')
+        # checking entity type is actually first elements
+        self.assertEqual(result[0].entity_type, 'ES_NIE')
+        # check the score ?
+        self.assertEqual(result[0].score, 0.85)
+
+        # negative test case
+        # first letter is not X,Y, or Z
         test_str = 'A1234567Z'
         result = analyze_text(test_str, ['ES_NIE'])
-        #expect empty list
+        # expect empty list
         self.assertEqual(len(result), 0)
-        #context enhancement
 
     def test_es_nif(self):
         """Test ES_NIF functionality"""
