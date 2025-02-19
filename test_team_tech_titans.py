6e0a7307923b4a68885350dbc20925e36f8f7a4b
AaniyahG <124090693+AaniyahG@users.noreply.github.com>
2025-02-19 21:55:10 +0000

    US_DRIVER_LICENSE context enhancers

--- test_team_tech_titans.py
+++ test_team_tech_titans.py
@@ -15,13 +15,39 @@ class TestTeam_tech_titans(unittest.TestCase):
     def test_us_bank_number(self):
         """Test US_BANK_NUMBER functionality"""
 
-        
+
     def test_us_driver_license(self):
         """Test US_DRIVER_LICENSE functionality"""
+
         # positive test case
+        # making an example string
         test_str = "12345678"
         result = analyze_text(test_str, ['US_DRIVER_LICENSE'])
-        print(result)
+        #expect result
+        self.assertGreater(len(result), 0, 'Result is empty')
+        #check correct entity type
+        self.assertEqual(result[0].entity_type, 'US_DRIVER_LICENSE')
+        #check score
+        self.assertEqual(result[0].score, .01)
+
+        #context enhance
+        #adding context word
+        test_str = 'license ' + test_str
+        result = analyze_text(test_str, ['US_DRIVER_LICENSE'])
+        #expect result
+        self.assertGreater(len(result), 0, 'Result is empty')
+        #check correct entity type
+        self.assertEqual(result[0].entity_type, 'US_DRIVER_LICENSE')
+        #check score
+        self.assertEqual(result[0].score, 0.4)
+        
+        # negative test case
+        # too short
+        test_str = "1234"
+        result = analyze_text(test_str, ['US_DRIVER_LICENSE'])
+        # expect empty list
+        self.assertEqual(len(result), 0)
+
 
     def test_us_itin(self):
         """Test US_ITIN functionality"""
