61132000c311c05f0712f6a9338fe064cea2849a
Iyan Tookes <108554547+SoulyRouly@users.noreply.github.com>
2025-02-21 01:26:06 +0000

    fi_personal_identity_code test cases along with the context enhancements

--- test_team_cmd.py
+++ test_team_cmd.py
@@ -16,33 +16,46 @@ class TestTeam_cmd(unittest.TestCase):
         """Test ES_NIF functionality"""
 
     def test_fi_personal_identity_code(self):
-        """Test FI_PERSONAL_IDENTITY_CODE functionality"""
-        
+        """Test FI_PERSONAL_IDENTITY_CODE functionality"""  
         # Example FI PIDs:
         #     VALID: 131052-308T
         #     041404A123X
         #     VALID: 150395-123K
-        
         # positive test case
-
         date = "131052"
         separator_char = '-'
         indiv_num = '308T'
         control_char = 'T'
         test_str = date + separator_char+ indiv_num + control_char
-        
         results = analyze_text(test_str,['FI_PERSONAL_IDENTITY_CODE'])
         #expect a result
         self.assertGreater(len(results),0,'Result is Empty :(')
         #check correct entity type
         self.assertEqual(results[0].entity_type,'FI_PERSONAL_IDENTITY_CODE')
+       # Check the score
+        self.assertEqual(results[0].score, 1)
+
+
+       # context enhancement(adding a context word)
+        """ 
+        None of  the context words(CW) actually improve the score. W/o any of the CWs the test string preforms fine(score =1.0). 
+        With the CWs however, the original score drops to 0.1 and the CW INCREASES the score to .9. Bringing it back to 1.0...
+        """
+
+        context_words = ["hetu", "henkilötunnus", "personbeteckningen", "personal identity code"]
+        test_str = context_words[0] +  test_str
+        results = analyze_text(test_str,['FI_PERSONAL_IDENTITY_CODE'])
+        #expect a result
+        self.assertGreater(len(results),0,'Result is Empty :(')
+        #check correct entity type
+        self.assertEqual(results[0].entity_type,'FI_PERSONAL_IDENTITY_CODE')
+       # Check the score
+        self.assertEqual(results[0].score, 1)
 
        # negative test case
-
         test_str = "041404A123X"
         results = analyze_text(test_str,['FI_PERSONAL_IDENTITY_CODE'])
         self.assertEqual(len(results),0)
-       # context enhancement
 
     def test_iban_code(self):
         """Test IBAN_CODE functionality"""
